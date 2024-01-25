ee6be7d1b5d8667d33fb7790ce93edd4ae352ad8
sajal1123 <113067506+sajal1123@users.noreply.github.com>
2024-01-25 17:22:29 -0600

    more testing for summary article

--- LLM/llama-indexer.py
+++ LLM/llama-indexer.py
@@ -1,20 +1,25 @@
 import os
+from llama_index.llms import Replicate
+from llama_index import set_global_tokenizer
+from transformers import AutoTokenizer
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index import ServiceContext
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 os.environ["REPLICATE_API_TOKEN"] = "r8_BOalg4ka8u21B7pF6DPcoRlxVfoKAHD3H7a8z"
 
-from llama_index.llms import Replicate
 
 llama2_7b_chat = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
 llm = Replicate(
     model=llama2_7b_chat,
     temperature=0.75,
-    additional_kwargs={"top_p": 1, "max_new_tokens": 30000, "system_prompt": "You are a reporter that provides crisp summaries of the last day's events."},
+    additional_kwargs={"top_p": 0.5,
+                       "max_new_tokens": 10000000, 
+                       "system_prompt": "You are a reporter that provides crisp summaries of the last day's events in 2500 to 2700 words."},
 )
 
 print("set tokenizer to match LLM...")
 
-from llama_index import set_global_tokenizer
-from transformers import AutoTokenizer
 
 set_global_tokenizer(
     AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-chat-hf").encode
@@ -22,8 +27,6 @@ set_global_tokenizer(
 
 print("global tokenizer set!!!")
 
-from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index import ServiceContext
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 service_context = ServiceContext.from_defaults(
@@ -31,7 +34,6 @@ service_context = ServiceContext.from_defaults(
 )
 # print("service context created\n\n", service_context)
 
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 print('creating index...')
 
@@ -56,6 +58,8 @@ summary = query_engine.query("Based on the provided data, write a 2500-2700 word
     You can omit less relevant news and add some creativity in your presentation, but make sure that you keep the important details\
         (like names, dates, and other information)in your write up. \. Make sure that you write this summary as if you are a reporter narrating the news.")
 
+stocks = query_engine.query("Tell me about the stock market. Is it a good time to invest?")
+
 # print("Document : \n\n\n", documents)
 # print("The Greatest Footballer Ever - \n\n", response1)
 # print("Messi's achievements - \n\n", response2)
@@ -67,3 +71,8 @@ summary = query_engine.query("Based on the provided data, write a 2500-2700 word
 
 print("\n\n\nSUMMARY OF THE DAY\n\n\n")
 summary.print_response_stream()
+
+print("\n\nSTOCKS\n\n", stocks)
+
+stanford = query_engine.query("Was there any news about Stanford?")
+print("\n\nSTANFORD\n\n", stanford)
